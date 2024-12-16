e5ab14770b4bca414a9819ac205775d3a7a4e7fd
Shreyansh Arora <shreyansharora69@gmail.com>
2024-12-17 02:20:39 +0530

    OurDomains done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Herosection from '../app/components/Herosection'; 
 import Footer from './components/Footer';
-
+import OurDomains from './components/OurDomains';
 export default function Home() {
   return (
     <div>
       <Herosection />
+      <OurDomains/>
       <Footer/>
     </div>
   );
