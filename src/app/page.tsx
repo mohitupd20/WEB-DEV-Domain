41cf92a3d50a94cc65899c6f040c0269545cdfde
Shreyansh Arora <shreyansharora69@gmail.com>
2024-12-17 18:23:50 +0530

    incoming fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Herosection from '../app/components/Herosection'; 
 import Footer from './components/Footer';
 import OurDomains from './components/OurDomains';
@@ -7,19 +6,9 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
-=======
-import Herosection from "../app/components/Herosection";
-import Footer from "./components/Footer";
-import OurDomains from "./components/OurDomains";
-import Navbar from "./components/Navbar";
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
->>>>>>> 19276d510378cb487f1bfaa2a7ff4d143562af9c
       <Herosection />
-      <OurDomains />
-      <Footer />
+      <OurDomains/>
+      <Footer/>
     </div>
   );
 }
