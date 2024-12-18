48818fde9833867c634c500963d168a3e5503936
Ananya Agrawal <ananya.agra02@gmail.com>
2024-12-18 21:20:46 +0400

    About Us initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Herosection from '../app/components/Herosection';
 import Footer from './components/Footer';
 import OurDomains from './components/OurDomains';
 import Navbar from './components/Navbar';
+import AboutUs from './components/AboutUs';
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Herosection />
+      <AboutUs/>
       <OurDomains/>
       <Footer/>
     </div>
